748e8e322e947443dec208542103e9cc151b4931
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-24 09:41:37 -0700

    Auth firebase working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import "./App.css";
+import ChatBox from "./components/chatBox";
+import { app } from "./components/firebase";
 
-import { initializeApp } from "firebase/app";
-import {} from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyAqLU_gEUgPfO_AJOc98B0k2hkAJP2x3Ko",
-  authDomain: "sall-b059b.firebaseapp.com",
-  projectId: "sall-b059b",
-  storageBucket: "sall-b059b.appspot.com",
-  messagingSenderId: "933299375895",
-  appId: "1:933299375895:web:c908895ae368f1a265b152"
-};
-const app = initializeApp(firebaseConfig);
+import { firestore } from "firebase/firestore";
+import { initializeAuth } from "firebase/auth";
+import "firebase/analytics";
 
-// getApp(app);
-// const db = getFirestore(app);
-// const auth = getAuth(app);
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const auth = initializeAuth(app);
+// const firestore = firestore();
+// const analytics = analytics();
 
 function App() {
-  // const [user] = useAuthState(auth);s
+  const [user] = useAuthState(auth);
 
   return (
     <div className="App">
       <header className="App-header">
+        {/* <div>other</div> */}
         {/* <section>{user ? <ChatBox /> : "sign ins"}</section> */}
         {/* <SignIn /> */}
         Sally
---
+++ src/components/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAqLU_gEUgPfO_AJOc98B0k2hkAJP2x3Ko",
+  authDomain: "sall-b059b.firebaseapp.com",
+  projectId: "sall-b059b",
+  storageBucket: "sall-b059b.appspot.com",
+  messagingSenderId: "933299375895",
+  appId: "1:933299375895:web:c908895ae368f1a265b152"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
