c58b1b43721e62f0fdef20385e6b7ffe93396da4
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-24 10:21:04 -0700

    Add react router dom

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { GoogleButton } from "react-google-button";
+
 import { UserAuth } from "../providers/GoogleAuthProvider";
 import { useNavigate } from "react-router-dom";
 
@@ -25,16 +25,10 @@ const Signin = () => {
     <div>
       <h1 className="text-center text-3xl font-bold py-8">Sign in</h1>
       <div className="max-w-[240px] m-auto py-4">
-        <GoogleButton onClick={handleGoogleSignIn} />
+        <button onClick={handleGoogleSignIn}>Sign in</button>
       </div>
     </div>
   );
 };
 
 export default Signin;
-
-// return (
-//   <>
-//     <button onClick={signInWithGoogle}>Sign In</button>
-//   </>
-// );
