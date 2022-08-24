832854f72bd9ee3e6c45362ed3a72c1ce9b8f507
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-24 15:35:38 -0700

    i tabler icons

--- src/components/sendMessage.jsx
+++ src/components/sendMessage.jsx
@@ -1,9 +1,46 @@
-const SendMessage = () => {
+import {
+  TextInput,
+  TextInputProps,
+  ActionIcon,
+  useMantineTheme
+} from "@mantine/core";
+import { IconSearch, IconArrowRight, IconArrowLeft } from "@tabler/icons";
+
+export function SendMessage() {
+  // const theme = useMantineTheme();
+
   return (
-    <>
-      <div>Send Message</div>
-    </>
+    <TextInput
+      icon={<IconSearch size={18} stroke={1.5} />}
+      radius="xl"
+      size="md"
+      rightSection={
+        <ActionIcon
+          size={32}
+          radius="xl"
+          // color={theme.primaryColor}
+          variant="filled"
+        >
+          {/* {theme.dir === "ltr" ? (
+            <IconArrowRight size={18} stroke={1.5} />
+          ) : (
+            <IconArrowLeft size={18} stroke={1.5} />
+          )} */}
+        </ActionIcon>
+      }
+      placeholder="Search questions"
+      rightSectionWidth={42}
+      // {...props}
+    />
   );
-};
+}
+
+// const SendMessage = () => {
+//   return (
+//     <>
+//       <div>Send Message</div>
+//     </>
+//   );
+// };
 
 export default SendMessage;
