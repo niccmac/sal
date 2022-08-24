113c6bda246b6727fd895fae5b708d787ac1e025
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-24 09:29:25 -0700

    No errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import "./App.css";
 
+import { initializeApp } from "firebase/app";
+import {} from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAqLU_gEUgPfO_AJOc98B0k2hkAJP2x3Ko",
+  authDomain: "sall-b059b.firebaseapp.com",
+  projectId: "sall-b059b",
+  storageBucket: "sall-b059b.appspot.com",
+  messagingSenderId: "933299375895",
+  appId: "1:933299375895:web:c908895ae368f1a265b152"
+};
+const app = initializeApp(firebaseConfig);
+
+// getApp(app);
+// const db = getFirestore(app);
+// const auth = getAuth(app);
+
 function App() {
-  // const [user] = useAuthState(auth);
+  // const [user] = useAuthState(auth);s
 
   return (
     <div className="App">
       <header className="App-header">
         {/* <section>{user ? <ChatBox /> : "sign ins"}</section> */}
-        <SignIn />
-        {/* Sally */}
+        {/* <SignIn /> */}
+        Sally
       </header>
     </div>
   );
--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,15 +1,34 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-// import ChatBox from "./components/chatBox";
-import SignIn from "./components/signin";
-// import ChatMessage from "./components/chat";
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
+// import "firebase/compat/firestore";
+// // import ChatBox from "./components/chatBox";
+// import SignIn from "./components/signin";
+// // import ChatMessage from "./components/chat";
 
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import { useCollectionData } from "react-firebase-hooks/firestore";
+// // import { useAuthState } from "react-firebase-hooks/auth";
+// // import { useCollectionData } from "react-firebase-hooks/firestore";
+// import { initializeApp } from "firebase/app";
+// // import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAqLU_gEUgPfO_AJOc98B0k2hkAJP2x3Ko",
+//   authDomain: "sall-b059b.firebaseapp.com",
+//   projectId: "sall-b059b",
+//   storageBucket: "sall-b059b.appspot.com",
+//   messagingSenderId: "933299375895",
+//   appId: "1:933299375895:web:c908895ae368f1a265b152"
+// };
+// const app = firebase.initializeApp(firebaseConfig);
+
+// const db = getFirestore(app);
+
+// const auth = firebase.auth();
+// const firestore = firebase.firestore();
+// const analytics = firebase.analytics();
+
 import { initializeApp } from "firebase/app";
-// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAqLU_gEUgPfO_AJOc98B0k2hkAJP2x3Ko",
   authDomain: "sall-b059b.firebaseapp.com",
@@ -18,10 +37,7 @@ const firebaseConfig = {
   messagingSenderId: "933299375895",
   appId: "1:933299375895:web:c908895ae368f1a265b152"
 };
-const app = firebase.initializeApp(firebaseConfig);
 
-// const db = getFirestore(app);
+const app = initializeApp(firebaseConfig);
 
-// const auth = firebase.auth();
-// const firestore = firebase.firestore();
-// const analytics = firebase.analytics();
+export { app };
