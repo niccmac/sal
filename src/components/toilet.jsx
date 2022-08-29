8c8a0138578a8f0e5d9f3749ec2e369d911b7514
Nicole MacLean <nicole.mac0404@gmail.com>
2022-08-29 09:54:57 -0700

    Query comment

--- src/components/toilet.jsx
+++ src/components/toilet.jsx
@@ -1,11 +1,42 @@
-import { Button } from "@mantine/core";
 import { IconDroplet, IconPoo } from "@tabler/icons";
 import { TimeInput } from "@mantine/dates";
 import { IconClock } from "@tabler/icons";
-import { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Drawer, Button, Group } from "@mantine/core";
+import { db, app } from "../firebase";
+// import firebase from "firebase";
+import {
+  query,
+  collection,
+  orderBy,
+  onSnapshot,
+  getDocs,
+  limit
+} from "firebase/firestore";
 const Toilet = () => {
   const [pee, setPee] = useState();
+  const [poo, setPoo] = useState();
   const brown = "#A52A2A";
+
+  const handlePee = () => {
+    const q = query(collection(db, "Pee"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      console.log(querySnapshot);
+      setPee("changed");
+      console.log("changed");
+    });
+    return () => unsubscribe();
+  };
+  const handlePoo = () => {
+    const q = query(collection(db, "Poo"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      console.log(querySnapshot);
+      setPee("changed");
+      console.log("changed");
+    });
+    return () => unsubscribe();
+  };
+
   return (
     <>
       <TimeInput
@@ -20,6 +51,7 @@ const Toilet = () => {
         size="md"
         compact
         leftIcon={<IconDroplet size={14} />}
+        onClick={() => handlePee()}
       >
         Pee
       </Button>
@@ -28,6 +60,7 @@ const Toilet = () => {
         placeholder="Pick time"
         icon={<IconClock size={16} />}
         defaultValue={new Date()}
+        onClick={() => handlePoo()}
       />
       <Button
         color="brown"
