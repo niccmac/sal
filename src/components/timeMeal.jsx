845b3cacb7908319ede4d97359b23b897892928d
Nicole MacLean <nicole.mac0404@gmail.com>
2022-09-01 12:23:41 -0700

    try to update eating active value

--- src/components/timeMeal.jsx
+++ src/components/timeMeal.jsx
@@ -2,26 +2,38 @@ import { useState } from "react";
 import { ActionIcon, Collapse } from "@mantine/core";
 import { IconDogBowl, IconTrashX } from "@tabler/icons";
 import { getDatabase, ref, set } from "firebase/database";
+import { collection } from "firebase/firestore";
 import { db } from "../firebase";
 
 const MealTime = (item) => {
-  console.log(item);
+  console.log("...", item);
   const [opened, setOpened] = useState(false);
   const { text, time } = item.item;
   let localTime = new Date(time.timestampValue);
   const displayTime = localTime.toString().substring(16, 21);
 
-  const handleTrashClick = (e) => {
+  const handleTrashClick = async (e) => {
     const database = getDatabase();
-    set(ref(database, "Eating" + "s3LBC8njrlgK2NXDukDQ"), {
-      active: false
-    })
-      .then(() => {
-        console.log("Updated");
-      })
-      .catch((e) => {
-        console.log("Not Updated", e);
-      });
+
+    // await database
+    //   .collection("Eating")
+    //   .doc(item.itemKey)
+    //   .update({ active: false })
+    //   .then((_) => console.info("Success"))
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
+    // set(ref(database, "Eating" + item.itemKey), {
+    //   text: text.stringValue,
+    //   time: time.timestampValue,
+    //   active: false
+    // })
+    // .then(() => {
+    //   console.log("Updated");
+    // })
+    // .catch((e) => {
+    //   console.log("Not Updated", e);
+    // });
     console.log("Trash Clicked");
   };
 
